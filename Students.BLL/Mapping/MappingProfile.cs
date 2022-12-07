ca48e6f231f877d012d39b89355277c45fbc87b1
Jamaltscode <jamalts@code.edu.az>
2022-12-08 00:06:14 +0400

    Repo task has done

---
+++ Students.BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using University.BLL.Dtos;
+using University.DAL.Entities;
+
+namespace University.BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Student, StudentDto>().ReverseMap();
+            CreateMap<Student, StudentCreateDto>().ReverseMap();
+           
+        }
+    }
+}
---
+++ Students.DAL/DataContext/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using University.DAL.Entities;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace University.DAL.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Student> Students { get; set; }
+    }
+}
